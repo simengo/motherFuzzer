8626758082b40753f89b74addffd28773356ca9d
MBreit <s8mibrei@stud.uni-saarland.de>
2018-09-05 11:33:41 +0200

    Logger angefangen und World getAnts verbessert

--- src/main/JSONLogger.java
+++ src/main/JSONLogger.java
@@ -1,11 +1,18 @@
+import java.io.File;
+import java.io.FileWriter;
+
 import java.util.List;
+import java.util.Map;
 import javax.json.*;
 
 public class JSONLogger implements Logger {
 
+    File output;
 
 
-    public JSONLogger(){
+    public JSONLogger(File protocol){
+
+        this.output = protocol;
 
 
     }
@@ -13,20 +20,49 @@ public class JSONLogger implements Logger {
 
 
     @Override
-    public void addInitialRound(Field[][] map) {
+    public void addInitialRound(Field[][] map, Map<Character, Integer> points) {
+
+
+        JsonObject model = Json.createObjectBuilder()
+            .add("firstName", "Duke")
+            .add("lastName", "Java")
+            .add("age", 18)
+            .add("streetAddress", "100 Internet Dr")
+            .add("city", "JavaTown")
+            .add("state", "JA")
+            .add("postalCode", "12345")
+            .add("phoneNumbers", Json.createArrayBuilder()
+                .add(Json.createObjectBuilder()
+                    .add("type", "mobile")
+                    .add("number", "111-111-1111"))
+                .add(Json.createObjectBuilder()
+                    .add("type", "home")
+                    .add("number", "222-222-2222")))
+            .build();
+
+
 
     }
+
 
 
     @Override
-    public void addRoundInfo(List<Field> changes) {
+    public void addRoundInfo(List<Field> changes,Map<Character, Integer> points) {
+
+
+
 
     }
 
 
+    private JSONObject createFieldsArray (Field[][] fields){
+
+
+    }
 
     @Override
     public void writeToFile() {
+
 
     }
 
