67ef91eb59f85d757694e3cbf4274bc4d4341adf
MilanConrad <s8miconr@stud.uni-saarland.de>
2018-09-06 15:28:30 +0200

    Main/Game PMD Bugfixing

---
+++ src/jUnitTest/MoveTests.java
@@ -0,0 +1,19 @@
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import saarland.cispa.sopra.*;
+
+
+public class MoveTests {
+
+
+
+    @Test
+    public void moveTest1(){
+
+
+
+
+    }
+}
