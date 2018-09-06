5bd184190bdaa2bef4d4a27b0d8d360f1fae32d5
Simon Engel <s8sgenge@stud.uni-saarland.de>
2018-09-06 17:34:44 +0200

    Move Resttime lol

---
+++ src/test/BreedTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import saarland.cispa.sopra.*;
+
+public class BreedTest {
+
+    @BeforeEach
+    public void doBefore () {
+    }
+
+    @BeforeAll
+    public void doBeforeAll () {
+        // Do nothing
+    }
+
+    @Test
+    public void emptyTest() {
+        // Do nothing
+    }
+
+}
---
+++ src/test/WorldPaserTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class WorldPaserTest {
+    public class BreedTest {
+
+        @BeforeEach
+        public void doBefore () {
+            // Do nothing
+        }
+
+        @BeforeAll
+        public void doBeforeAll () {
+            //Do nothing
+        }
+
+        @Test
+        public void emptyTest() {
+            // Do nothing
+        }
+
+    }
+
+}
