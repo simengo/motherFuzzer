6db272ce9e6f1777f112d8805c5f84493995494c
Simon Engel <s8sgenge@stud.uni-saarland.de>
2018-09-07 14:01:14 +0200

    worldParserTest

--- src/test/WorldPaserTest.java
+++ src/test/WorldPaserTest.java
@@ -1,7 +1,11 @@
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import saarland.cispa.sopra.*;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class WorldPaserTest {
@@ -9,9 +13,23 @@ public class WorldPaserTest {
 
         @BeforeEach
         public void doBefore () {
-            // Do nothing
+            Map<Character, Swarm> swarms = new HashMap<Character,Swarm>();
+            Instruction[] brainA = new Instruction[1];
+            Instruction[] brainB = new Instruction[1];
+            brainA[0] = new Jump(0);
+            brainB[0] = new Jump(0);
+            Swarm swarmA = new Swarm('A',brainA,"A");
+            Swarm swarmB = new Swarm('B',brainB,"B");
+            swarms.put('A',swarmA);
+            swarms.put('B',swarmB);
+            boolean fail = false;
 
-
+            try {
+                World world = WorldParser.parseMap(new File("C:\\Users\\simen\\Desktop\\map.txt"),42,swarms);
+            } catch (java.io.IOException e){
+                fail = true;
+            }
+            assert(fail);
         }
 
         @BeforeAll
