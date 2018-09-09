08aeedf8008b3381874b2b73897e11d5da993aca
MilanConrad <s8miconr@stud.uni-saarland.de>
2018-09-09 17:16:53 +0200

    Aktualisierter Breed (Killcheck hinzugefügt) SpawnAnts in Breed Rückgabewert Ant

--- src/jUnitTest/BreedTest1.java
+++ src/jUnitTest/BreedTest1.java
@@ -9,15 +9,11 @@ public class BreedTest1 {
     public void breedTest1() {
 
 
-        World welt = MoveTests.Szenario1();
+        World welt = MoveTests.Szenario3();
         welt.getAnt(1).setHasFood(true);
-        Ant antB1 = new Ant(welt.getAnt(1).getSwarmInstance(), 2, (Field) welt.getFieldAt(1, 0));
-        ((Field) welt.getFieldAt(1, 0)).setAnt(antB1);
-        welt.getAnts().add(antB1);
         welt.getAnt(2).setHasFood(true);
-
-        Instruction breed = new Breed(0);
-        breed.execute(welt, welt.getAnt(2));
+        Instruction breed = welt.getAnt(1).getNextInstruction();
+        breed.execute(welt, welt.getAnt(1));
 
         assert (welt.getAnt(3) != null && welt.getFieldAt(0, 1).getAnt().isPresent());
 
