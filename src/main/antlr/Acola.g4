e1f025a688638e0a35c2b51cc0606c612dc56870
Eric Petryka <s8erpetr@stud.uni-saarland.de>
2018-09-10 12:27:15 +0200

    error fixes

--- src/main/antlr/Acola.g4
+++ src/main/antlr/Acola.g4
@@ -1,40 +1,39 @@
 grammar Acola;
-brain : 'brain' '""' IDENTIFIER '""' '{' instruction+ '}' EOF;
+brain : 'brain' '"' IDENTIFIER '"' '{' '\n' (instruction|jump)* jump '}' EOF;
 instruction : mark
-              unmark
-              turn
-              move
-              sense
-              set
-              unset
-              pickup
-              drop
-              jump
-              flip
-              sensemarker
-              directions
-              test
-              COMMENT;
+              |unmark
+              |turn
+              |move
+              |sense
+              |set
+              |unset
+              |pickup
+              |drop
+              |jump
+              |flip
+              |sensemarker
+              |directions
+              |test
+              |COMMENT;
 
-mark: 'mark' NUMBER;
-unmark: 'unmark'NUMBER;
-turn: 'turn' TURNDIRECTION;
-move: 'move' 'else' NUMBER;
-sense: 'sense' FIELD TARGET 'else' NUMBER;
-sensemarker: 'sense' FIELD 'marker' MARKER 'else' NUMBER;
-set: 'set' NUMBER;
-unset: 'unset' NUMBER;
-pickup: 'pickup''else' NUMBER;
-drop: 'drop''else' NUMBER;
-jump: 'jump' NUMBER;
-flip: 'flip' 'else' NUMBER;
-test: 'test' 'else' NUMBER;
-directions: 'directions' DIRECTION;
+mark: 'mark' NUMBER '\n';
+unmark: 'unmark' NUMBER '\n';
+turn: 'turn' ('left' | 'right') '\n';
+move: 'move' 'else' NUMBER '\n';
+sense: 'sense' FIELD TARGET 'else' NUMBER '\n';
+sensemarker: 'sense' FIELD 'marker' MARKER 'else' NUMBER '\n';
+set: 'set' NUMBER '\n';
+unset: 'unset' NUMBER '\n';
+pickup: 'pickup''else' NUMBER '\n';
+drop: 'drop''else' NUMBER '\n';
+jump: 'jump' NUMBER '\n';
+flip: 'flip' 'else' NUMBER '\n';
+test: 'test' 'else' NUMBER '\n';
+directions: 'directions' DIRECTION '\n';
 
 FIELD : 'here' | 'ahead' | 'left' | 'right';
-TARGET : 'foe' | 'foehome' | 'friend' | 'food' | 'antlion' | 'rock' | 'foefood' | 'antlion' | 'foefood' | 'friendfood' | 'foemarker';
+TARGET : 'foe' | 'foehome' | 'friend' | 'food' | 'antlion' | 'rock' | 'foefood' | 'foemarker';
 DIRECTION :  'northwest' | 'west' | 'southwest' | 'southeast' | 'east' | 'northeast';
-TURNDIRECTION : 'left' | 'right';
 MARKER : [0-5];
 NUMBER : [0-9]+;
 IDENTIFIER : [a-zA-Z_.-][a-zA-Z0-9_.-]+;
--- src/main/java/saarland/cispa/sopra/BrainVisitor.java
+++ src/main/java/saarland/cispa/sopra/BrainVisitor.java
@@ -1,113 +1,35 @@
 package saarland.cispa.sopra;
 
-import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.tree.RuleNode;
-import org.antlr.v4.runtime.tree.TerminalNode;
+import saarland.cispa.sopra.antlr.AcolaBaseVisitor;
 import saarland.cispa.sopra.antlr.AcolaParser;
-import saarland.cispa.sopra.antlr.AcolaVisitor;
 
 import java.util.List;
 
-public class BrainVisitor implements AcolaVisitor {
+public class BrainVisitor extends AcolaBaseVisitor<String> {
 
     @Override
-    public List<AcolaParser.InstructionContext> visitBrain(AcolaParser.BrainContext ctx) {
-        return ctx.instruction();
+    public String visitBrain(AcolaParser.BrainContext ctx) {
+        List<AcolaParser.InstructionContext> list = ctx.instruction();
+        StringBuilder builder = new StringBuilder();
+        for (AcolaParser.InstructionContext context : list) {
+            builder.append(visitInstruction(context));
+        }
+        return builder.toString();
     }
 
     @Override
     public String visitInstruction(AcolaParser.InstructionContext ctx) {
-        return ctx.getText();
+        visitChildren(ctx);
+        StringBuilder result = new StringBuilder();
+        int num = ctx.getChildCount();
+        for (int iterator = 0; iterator < num; iterator++) {
+            ParseTree tree = ctx.getChild(iterator);
+            String childResult = tree.getText();
+            result.append(childResult);
+        }
+        return result.toString();
     }
 
-    @Override
-    public Object visitMark(AcolaParser.MarkContext ctx) {
-        return ctx.children;
-    }
 
-    @Override
-    public Object visitUnmark(AcolaParser.UnmarkContext ctx) {
-        return null;
-    }
-
-    @Override
-    public Object visitTurn(AcolaParser.TurnContext ctx) {
-        return null;
-    }
-
-    @Override
-    public Object visitMove(AcolaParser.MoveContext ctx) {
-        return null;
-    }
-
-    @Override
-    public Object visitSense(AcolaParser.SenseContext ctx) {
-        return null;
-    }
-
-    @Override
-    public Object visitSensemarker(AcolaParser.SensemarkerContext ctx) {
-        return null;
-    }
-
-    @Override
-    public Object visitSet(AcolaParser.SetContext ctx) {
-        return null;
-    }
-
-    @Override
-    public Object visitUnset(AcolaParser.UnsetContext ctx) {
-        return null;
-    }
-
-    @Override
-    public Object visitPickup(AcolaParser.PickupContext ctx) {
-        return null;
-    }
-
-    @Override
-    public Object visitDrop(AcolaParser.DropContext ctx) {
-        return null;
-    }
-
-    @Override
-    public Object visitJump(AcolaParser.JumpContext ctx) {
-        return null;
-    }
-
-    @Override
-    public Object visitFlip(AcolaParser.FlipContext ctx) {
-        return null;
-    }
-
-    @Override
-    public Object visitTest(AcolaParser.TestContext ctx) {
-        return null;
-    }
-
-    @Override
-    public Object visitDirections(AcolaParser.DirectionsContext ctx) {
-        return null;
-    }
-
-    @Override
-    public Object visit(ParseTree tree) {
-        return null;
-    }
-
-    @Override
-    public Object visitChildren(RuleNode node) {
-        return null;
-    }
-
-    @Override
-    public Object visitTerminal(TerminalNode node) {
-        return null;
-    }
-
-    @Override
-    public Object visitErrorNode(ErrorNode node) {
-        return null;
-    }
 }
