f90a054c05854c5a70ab5e54c2d6f398071ee9ac
MBreit <s8mibrei@stud.uni-saarland.de>
2018-09-10 13:40:57 +0200

    AntTests + FieldTests

--- src/test/AntTests.java
+++ src/test/AntTests.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 import saarland.cispa.sopra.*;
+import saarland.cispa.sopra.systemtests.WorldInfo;
 
 
 import java.util.HashMap;
@@ -10,19 +11,24 @@ public class AntTests {
     @Test
     public void emptyTest() {
 
-        Normal[][] spielfeld = new Normal[2][2];
-        Normal field00 = new Normal(0, 0, 0);
-        Normal field01 = new Normal(0, 1, 0);
-        Normal field10 = new Normal(1, 0, 0);
-        Normal field11 = new Normal(1, 1, 0);
+        Field[][] spielfeld = new Field[2][2];
+        Field field00 = new Base('A', 0, 0);
+        Field field01 = new Normal(0, 1, 0);
+        Field field10 = new Normal(1, 0, 9);
+        Field field11 = new Base('B', 1, 1);
         spielfeld[0][0] = field00;
         spielfeld[0][1] = field01;
         spielfeld[1][0] = field10;
         spielfeld[1][1] = field11;
-        Instruction[] brainA = new Instruction[1];
-        brainA[0] = new Jump(0);
+        Instruction[] brainA = new Instruction[3];
+        brainA[0] = new Set(3);
+        brainA[1] = new Set(5);
+        brainA[2] = new Jump(0);
         Swarm swarmA = new Swarm('A', brainA, "brainA");
         Swarm swarmB = new Swarm('B', brainA, "brainB");
+        HashMap<Character, Swarm> swarms = new HashMap<>();
+        swarms.put('A', swarmA);
+        swarms.put('B', swarmB);
         Ant antA = new Ant(swarmA, 0, spielfeld[0][0]);
         Ant antB = new Ant(swarmB, 1, spielfeld[1][1]);
         spielfeld[0][0].setAnt(antA);
@@ -33,6 +39,7 @@ public class AntTests {
         ants.put(0, antA);
         ants.put(1, antB);
 
+        World world = new World(spielfeld,12,ants, swarms);
         assert (antA.getSwarm() == 'A');
         assert (antA.getRegister(0) == false);
         assert (antA.getRegister(1) == false);
@@ -45,9 +52,30 @@ public class AntTests {
 
         assert(antB.getDirection() == "northwest");
         assert(antB.isDead() == false);
+        assert(antA.getId() == 0);
         assert(antB.getId() == 1);
         assert(antB.getPc() == 0);
         assert(antB.getRestTime() == 0);
+
+        antA.getNextInstruction().execute(world,antA);
+        antA.getNextInstruction().execute(world,antA);
+        antB.getNextInstruction().execute(world,antB);
+
+
+        assert (antA.getRegister(0) == false);
+        assert (antA.getRegister(1) == false);
+        assert (antA.getRegister(2) == false);
+        assert (antA.getRegister(3) == true);
+        assert (antA.getRegister(4) == false);
+        assert (antA.getRegister(5) == true);
+
+        assert (antB.getRegister(0) == false);
+        assert (antB.getRegister(1) == false);
+        assert (antB.getRegister(2) == false);
+        assert (antB.getRegister(3) == true);
+        assert (antB.getRegister(4) == false);
+        assert (antB.getRegister(5) == false);
+
 
 
 
