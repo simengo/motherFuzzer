b0d90ca77432f3d6f8f590f8e37dceea4a27abcd
MilanConrad <s8miconr@stud.uni-saarland.de>
2018-09-10 14:29:10 +0200

    WorldParser Remake

--- src/main/java/saarland/cispa/sopra/WorldParser.java
+++ src/main/java/saarland/cispa/sopra/WorldParser.java
@@ -17,150 +17,68 @@ public final class WorldParser {
 
     public static World parseMap(File mapFile, long seed, Map<Character, Swarm> swarms) throws IOException {
 
-        int x = 0;
-        int y = 0;
+        Field[][] fields = null;
 
         try (BufferedReader bReader = Files.newBufferedReader(Paths.get(mapFile.getPath()))) {
 
-            int counter = 0;
-            int width = Integer.parseInt(bReader.readLine());
-            int height = Integer.parseInt(bReader.readLine());
-            boolean newLine1 = false;
-            boolean ntoken = false;
-            boolean nexttoken = false;
-            boolean outbreak = false;
-            String breite1 = bReader.readLine();
-            char[] width1 = breite1.toCharArray();
-            int breite = 0;
-            int hoehe = 0;
+            String line = bReader.readLine();
 
-            for (char character : width1) {
-                if (outbreak) break;
-                switch (character) {
+            boolean heighttoken = false;
+            boolean widthtoken = false;
 
-                    case 92:
-                        newLine1 = true;
-                        break;
+            String[] splittedlines = line.split("\\\\n");
 
-                    case 'n':
-                        if (newLine1) {
-                            ntoken = true;
-                            break;
-                        } else {
-                            throw new IllegalArgumentException("Map could not be parsed correctly");
-                        }
+            int width = checkNumber(splittedlines[0].toCharArray());
+            int height = checkNumber(splittedlines[1].toCharArray());
 
-                    default:
-                        if (newLine1 && ntoken) {
-                            newLine1 = false;
-                            ntoken = false;
-                            if (nexttoken) {
-                                outbreak = true;
-                                break;
-                            }
-                            nexttoken = true;
-                            hoehe = hoehe * 10 + (character - 48);
-                            break;
+            if (splittedlines.length > height + 2)
+                throw new IllegalArgumentException("Map could not be parsed correctly");
 
-                        } else {
-                            if (newLine1 || ntoken) {
-                                throw new IllegalArgumentException("Map could not be parsed correctly");
-                            } else {
-                                if (nexttoken) {
-                                    hoehe = hoehe * 10 + (character - 48);
-                                    break;
-                                } else {
-                                    breite = breite * 10 + (character - 48);
-                                    break;
-                                }
-                            }
+            fields = new Field[width][height];
 
-                        }
+            for (int i = 2; i < splittedlines.length; i++) {
 
+                char[] actualLine = splittedlines[i].toCharArray();
 
+                int x = 0;
+
+                for (char actualChar : actualLine) {
+                    if (x >= width) {
+                        throw new IllegalArgumentException("Map could not be parsed correctly");
+                    }
+                    checkLetter(actualChar, fields, x, i - 2);
+                    x++;
                 }
             }
-            //      int hoehe = Integer.parseInt(bReader.readLine());
 
-            Field[][] fields = new Field[width][height];
-
-            while (true) {
-
-                String line = bReader.readLine();
-
-                if (line == null) {
-                    if (counter % 2 != 0 || counter == 0 || counter != height) {
-                        throw new IllegalArgumentException();
-                    }
-                    break;
-                }
-
-                char[] row = line.toCharArray();
-
-                boolean newLine = false;
-
-                for (Character chara : row) {
-
-                    switch (chara) {
-
-                        case 92:
-                            newLine = true;
-                            break;
-
-                        case 'n':
-                            if (newLine) {
-                                newLine = false;
-                                if (x != breite + 1) {
-                                    throw new IllegalArgumentException("Map could not be parsed correctly");
-                                }
-                                x = -1;
-                                y++;
-                                counter++;
-                                break;
-                            } else {
-                                throw new IllegalArgumentException("Map could not be parsed correctly (Invalid character)");
-                            }
-                        case '.':
-                            fields[x][y] = new Normal(x, y, 0);
-                            break;
-
-                        case '=':
-                            fields[x][y] = new Antlion(x, y);
-                            break;
-
-                        case '#':
-                            fields[x][y] = new Rock(x, y);
-                            break;
-
-                        default:
-                            checkLetter(chara, fields, x, y);
-                            break;
-                    }
-                    y++;
-                }
-                y = 0;
-                x++;
-                counter += 1;
-                if (x != breite || y >= hoehe) {
-                    throw new IllegalArgumentException("Map could not be parsed correctly");
-                }
-                x = 0;
-                y++;
-                counter++;
-            }
-            Map<Integer, Ant> ants = spawnAnts(swarms, fields);
-            World welt = new World(fields, seed, ants, swarms);
-            welt.setAntlion();
-            return welt;
         }
+
+        Map<Integer, Ant> ants = spawnAnts(swarms, fields);
+        World welt = new World(fields, seed, ants, swarms);
+        welt.setAntlion();
+        return welt;
     }
 
+
     private static void checkLetter(char fieldType, Field[][] fields, int x, int y) {
+
+
         if (fieldType >= 65 && fieldType <= 90 || fieldType >= 97 && fieldType <= 122) {
             fields[x][y] = new Base(fieldType, x, y);
+            return;
         }
         if (fieldType >= 49 && fieldType <= 57) {
             fields[x][y] = new Normal(x, y, fieldType - 48);
+            return;
+        }
+
+        if (fieldType == '.') {
+            fields[x][y] = new Normal(x, y, 0);
+            return;
+        }
+        if (fieldType == '=') {
+            fields[x][y] = new Antlion(x, y);
+            return;
         } else {
             throw new IllegalArgumentException("Map could not be parsed correctly (Invalid Character)");
         }
@@ -182,5 +100,18 @@ public final class WorldParser {
             }
         }
         return ants;
+    }
+
+    private static int checkNumber(char[] number) {
+
+        int result = 0;
+        for (char digit : number) {
+            if (digit < 48 || digit > 57) {
+                throw new IllegalArgumentException("Map could not be parsed correctly : Illegal Header");
+            } else {
+                result = result * 10 + digit - 48;
+            }
+        }
+        return result;
     }
 }
