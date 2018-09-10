5cbc82feba30e69e4473e266f8a2eff810377b8d
MilanConrad <s8miconr@stud.uni-saarland.de>
2018-09-10 10:48:27 +0200

    Aktualisierter Breed (Killcheck hinzugefügt) SpawnAnts in Breed Rückgabewert Ant

--- src/main/java/saarland/cispa/sopra/WorldParser.java
+++ src/main/java/saarland/cispa/sopra/WorldParser.java
@@ -21,13 +21,69 @@ public final class WorldParser {
         int jPMDlaenger = 0;
 
         try (BufferedReader bReader = Files.newBufferedReader(Paths.get(mapFile.getPath()))) {
+
             int counter = 0;
-            int breite = Integer.parseInt(bReader.readLine());
-            int hoehe = Integer.parseInt(bReader.readLine());
+            boolean newLine1 = false;
+            boolean ntoken = false;
+            boolean nexttoken = false;
+            boolean outbreak = false;
+            String breite1 = bReader.readLine();
+            char[] width = breite1.toCharArray();
+            int breite = 0;
+            int hoehe = 0;
+
+            for (char x : width) {
+                if (outbreak) break;
+                switch (x) {
+
+                    case 92:
+                        newLine1 = true;
+                        break;
+
+                    case 'n':
+                        if (newLine1) {
+                            ntoken = true;
+                            break;
+                        } else {
+                            throw new IllegalArgumentException("Map could not be parsed correctly");
+                        }
+
+                    default:
+                        if (newLine1 && ntoken) {
+                            newLine1 = false;
+                            ntoken = false;
+                            if (nexttoken) {
+                                outbreak = true;
+                                break;
+                            }
+                            nexttoken = true;
+                            hoehe = hoehe * 10 + (x - 48);
+                            break;
+
+                        } else {
+                            if (newLine1 || ntoken) {
+                                throw new IllegalArgumentException("Map could not be parsed correctly");
+                            } else {
+                                if (nexttoken) {
+                                    hoehe = hoehe * 10 + (x - 48);
+                                    break;
+                                } else {
+                                    breite = breite * 10 + (x - 48);
+                                    break;
+                                }
+                            }
+
+                        }
+
+
+                }
+            }
+      //      int hoehe = Integer.parseInt(bReader.readLine());
 
             Field[][] fields = new Field[breite][hoehe];
 
             while (true) {
+
                 String line = bReader.readLine();
 
                 if (line == null) {
@@ -39,11 +95,29 @@ public final class WorldParser {
 
                 char[] row = line.toCharArray();
 
-                if (row.length != breite) {
-                    throw new IllegalArgumentException("Width doesnt match head");
-                }
+                boolean newLine = false;
+
                 for (Character chara : row) {
+
                     switch (chara) {
+
+                        case 92:
+                            newLine = true;
+                            break;
+
+                        case 'n':
+                            if (newLine) {
+                                newLine = false;
+                                if (jPMDlaenger != breite + 1) {
+                                    throw new IllegalArgumentException("Map could not be parsed correctly");
+                                }
+                                jPMDlaenger = -1;
+                                iPMDleanger++;
+                                counter++;
+                                break;
+                            } else {
+                                throw new IllegalArgumentException("Map could not be parsed correctly (Invalid character)");
+                            }
                         case '.':
                             fields[iPMDleanger][jPMDlaenger] = new Normal(iPMDleanger, jPMDlaenger, 0);
                             break;
@@ -70,9 +144,12 @@ public final class WorldParser {
                     }
                     jPMDlaenger++;
                 }
+                if (jPMDlaenger != breite || iPMDleanger >= hoehe) {
+                    throw new IllegalArgumentException("Map could not be parsed correctly");
+                }
                 jPMDlaenger = 0;
                 iPMDleanger++;
-                counter += 1;
+                counter++;
             }
             Map<Integer, Ant> ants = spawnAnts(swarms, fields);
             World welt = new World(fields, seed, ants, swarms);
