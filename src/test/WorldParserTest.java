a6be8ffc9957e9db3c7678e7963f48b14158c178
Simon Engel <s8sgenge@stud.uni-saarland.de>
2018-09-10 11:52:54 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/saarland/cispa/sopra/WorldParser.java

--- src/test/WorldParserTest.java
+++ src/test/WorldParserTest.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 public class WorldParserTest {
         @Test
-        public void emptyTest() {
+        public void smallMapTest() {
             Map<Character, Swarm> swarms = new HashMap<Character,Swarm>();
             Instruction[] brainA = new Instruction[1];
             Instruction[] brainB = new Instruction[1];
@@ -23,12 +23,135 @@ public class WorldParserTest {
             boolean fail = false;
 
             try {
-                World world = WorldParser.parseMap(new File("C:\\Users\\simen\\Desktop\\map.txt"),42,swarms);
+                World world = WorldParser.parseMap(new File("C:\\Users\\simen\\Desktop\\maps\\smallMap.txt"),42,swarms);
             } catch (java.io.IOException e){
                 fail = true;
             }
             assert(!fail);
         }
 
+    @Test
+    public void bigMapTest() {
+        Map<Character, Swarm> swarms = new HashMap<Character,Swarm>();
+        Instruction[] brainA = new Instruction[1];
+        Instruction[] brainB = new Instruction[1];
+        brainA[0] = new Jump(0);
+        brainB[0] = new Jump(0);
+        Swarm swarmA = new Swarm('A',brainA,"A");
+        Swarm swarmB = new Swarm('B',brainB,"B");
+        swarms.put('A',swarmA);
+        swarms.put('B',swarmB);
+        boolean fail = false;
+
+        try {
+            World world = WorldParser.parseMap(new File("C:\\Users\\simen\\Desktop\\maps\\bigMap.txt"),42,swarms);
+        } catch (java.io.IOException e){
+            fail = true;
+        }
+        assert(!fail);
+    }
+
+    @Test
+    public void unevenMapTest() {
+        Map<Character, Swarm> swarms = new HashMap<Character,Swarm>();
+        Instruction[] brainA = new Instruction[1];
+        Instruction[] brainB = new Instruction[1];
+        brainA[0] = new Jump(0);
+        brainB[0] = new Jump(0);
+        Swarm swarmA = new Swarm('A',brainA,"A");
+        Swarm swarmB = new Swarm('B',brainB,"B");
+        swarms.put('A',swarmA);
+        swarms.put('B',swarmB);
+        boolean fail = false;
+
+        try {
+            World world = WorldParser.parseMap(new File("C:\\Users\\simen\\Desktop\\maps\\unevenMap.txt"),42,swarms);
+        } catch (java.io.IOException e){
+            fail = true;
+        }
+        assert(fail);
+    }
+
+    @Test
+    public void emptyMapTest() {
+        Map<Character, Swarm> swarms = new HashMap<Character,Swarm>();
+        Instruction[] brainA = new Instruction[1];
+        Instruction[] brainB = new Instruction[1];
+        brainA[0] = new Jump(0);
+        brainB[0] = new Jump(0);
+        Swarm swarmA = new Swarm('A',brainA,"A");
+        Swarm swarmB = new Swarm('B',brainB,"B");
+        swarms.put('A',swarmA);
+        swarms.put('B',swarmB);
+        boolean fail = false;
+
+        try {
+            World world = WorldParser.parseMap(new File("C:\\Users\\simen\\Desktop\\maps\\emptyMap.txt"),42,swarms);
+        } catch (java.io.IOException e){
+            fail = true;
+        }
+        assert(fail);
+    }
+
+    @Test
+    public void tooBigMapTest() {
+        Map<Character, Swarm> swarms = new HashMap<Character,Swarm>();
+        Instruction[] brainA = new Instruction[1];
+        Instruction[] brainB = new Instruction[1];
+        brainA[0] = new Jump(0);
+        brainB[0] = new Jump(0);
+        Swarm swarmA = new Swarm('A',brainA,"A");
+        Swarm swarmB = new Swarm('B',brainB,"B");
+        swarms.put('A',swarmA);
+        swarms.put('B',swarmB);
+        boolean fail = false;
+
+        try {
+            World world = WorldParser.parseMap(new File("C:\\Users\\simen\\Desktop\\maps\\tooBigMap.txt"),42,swarms);
+        } catch (java.io.IOException e){
+            fail = true;
+        }
+        assert(fail);
+    }
+    @Test
+    public void wrongHeaderMapTest() {
+        Map<Character, Swarm> swarms = new HashMap<Character,Swarm>();
+        Instruction[] brainA = new Instruction[1];
+        Instruction[] brainB = new Instruction[1];
+        brainA[0] = new Jump(0);
+        brainB[0] = new Jump(0);
+        Swarm swarmA = new Swarm('A',brainA,"A");
+        Swarm swarmB = new Swarm('B',brainB,"B");
+        swarms.put('A',swarmA);
+        swarms.put('B',swarmB);
+        boolean fail = false;
+
+        try {
+            World world = WorldParser.parseMap(new File("C:\\Users\\simen\\Desktop\\maps\\wrongHeaderMap.txt"),42,swarms);
+        } catch (java.io.IOException e){
+            fail = true;
+        }
+        assert(!fail);
+    }
+    @Test
+    public void invalidCharsMapTest() {
+        Map<Character, Swarm> swarms = new HashMap<Character,Swarm>();
+        Instruction[] brainA = new Instruction[1];
+        Instruction[] brainB = new Instruction[1];
+        brainA[0] = new Jump(0);
+        brainB[0] = new Jump(0);
+        Swarm swarmA = new Swarm('A',brainA,"A");
+        Swarm swarmB = new Swarm('B',brainB,"B");
+        swarms.put('A',swarmA);
+        swarms.put('B',swarmB);
+        boolean fail = false;
+
+        try {
+            World world = WorldParser.parseMap(new File("C:\\Users\\simen\\Desktop\\maps\\invalidCharsMap.txt"),42,swarms);
+        } catch (java.io.IOException e){
+            fail = true;
+        }
+        assert(fail);
+    }
     }
 
