75b103839f883f86e66b8c620b52e6f478f5b963
Simon Engel <s8sgenge@stud.uni-saarland.de>
2018-09-19 12:28:09 +0200

    name fixes

--- src/test/test.java
+++ src/test/test.java
@@ -1,10 +1,19 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import saarland.cispa.sopra.Instruction;
+import saarland.cispa.sopra.InstructionGenerator;
+
+import java.util.ArrayList;
 
 public class test {
 
     @Test
-    public void test(){
-
+    public void InstructionGeneratorTest() {
+        ArrayList<Instruction> instructions = new ArrayList<>();
+        Instruction instruction;
+        for (int i = 0; i<10; i++){
+            instruction = InstructionGenerator.createInstruction(100);
+            instructions.add(instruction);
+        }
     }
 
 }
